f4726bdbfa31de65812f87b0f727b0d3587d6f93
Ash <ashleytfage@gmail.com>
2024-09-23 08:18:01 +1000

    started api file

--- Deniro/main.py
+++ Deniro/main.py
@@ -1,25 +1,34 @@
-from deniro import Conversation
+import threading
 import socket
+from deniro import Conversation
+
 
 host = ''
 port = 1212
-conv = Conversation()
+
+
+def handle(conn, addr):
+    conv = Conversation()
+
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024).decode()
+            if data:
+                conv.set_prompt(data)
+                conv.generate_response()
+                conv.speak()
 
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
-        conn, addr = s.accept()
 
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024).decode()
-                if data:
-                    conv.set_prompt(data)
-                    conv.generate_response()
-                    conv.speak()
+        while True:
+            conn, addr = s.accept()
+            thread = threading.Thread(target=handle, args=(conn, addr))
+            thread.start()
 
 
 if __name__ == '__main__':
