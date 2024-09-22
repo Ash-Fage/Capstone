11475ea41aea891c5d781aa27ec159eba774073e
Ash <ashleytfage@gmail.com>
2024-09-23 05:49:31 +1000

    added server

--- Deniro/main.py
+++ Deniro/main.py
@@ -1,14 +1,25 @@
-from transcription import Transcription
+from deniro import Conversation
+import socket
+
+host = ''
+port = 1212
+conv = Conversation()
 
 
 def main():
-    '''test = Transcription()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        conn, addr = s.accept()
 
-    while True:
-        test.record()
-        print(test.transcribe().strip(), "\n")
-        test.denirotalk()
-        test.speak()'''
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024).decode()
+                if data:
+                    conv.set_prompt(data)
+                    conv.generate_response()
+                    conv.speak()
 
 
 if __name__ == '__main__':
